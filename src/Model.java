0cb2c1263473f8ce51cc7829aa7376a0080cbc62
Robert Newey <robert@nuiz.org>
2014-05-30 09:41:18 +1000

    Removed old, out of date default package stuff

--- src/Model.java
+++
@@ -1,8 +0,0 @@
-import java.util.Vector;
-
-
-public interface Model {
-	double doFold (final Vector<Rating> ratings, int fold);
-	
-	Vector <Double> getFit (final Vector<Rating> ratings, int fold);
-}
